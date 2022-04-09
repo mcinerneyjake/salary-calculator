0ad6205bea24d754efb527ae77009dfb2f2cbed3
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-09 09:46:49 -0500

    refactored append to table

--- salaryCalc.js
+++ salaryCalc.js
@@ -4,21 +4,50 @@ function onReady() {
   $('#submitButton').on('click', submitEmployee);
 }
 
+let employees = [];
+
 function submitEmployee() {
-  let firstName = $('#firstNameInput').val();
-  let lastName = $('#lastNameInput').val();
-  let idNumber = $('#idNumberInput').val();
-  let jobTitle = $('#jobTitleInput').val();
-  let annualSalary = $('#annualSalaryInput').val();
+  // Create employee object with necessary properties
+  let employeeObject = {
+    firstName: $('#firstNameInput').val(),
+    lastName: $('#lastNameInput').val(),
+    idNumber: $('#idNumberInput').val(),
+    jobTitle: $('#jobTitleInput').val(),
+    annualSalary: $('#annualSalaryInput').val(),
+  };
+
+  // Push employee object into employees array
+
+  employees.push(employeeObject);
+
+  // Loop through each entered employee
   let table = $('#tableRows');
 
-  table.append(`
+  for (let employee of employees) {
+    // Give each <td> tag a class
+    // Select the <td> class via jQuery
+    // Give the annualSalary a special class and add each appended annualSalary class
+    // together to get the total salary for all employees
+    table.append(`
   <tr>
-  <td>${firstName}</td>
-  <td>${lastName}</td>
-  <td>${idNumber}</td>
-  <td>${jobTitle}</td>
-  <td>${annualSalary}</td>
+  <td>${employee.firstName}</td>
+  <td>${employee.lastName}</td>
+  <td>${employee.idNumber}</td>
+  <td>${employee.jobTitle}</td>
+  <td>$${employee.annualSalary}</td>
   </tr>
   `);
+
+    // Empty employees array
+    employees = [];
+
+    // Empty text inputs
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idNumberInput').val('');
+    $('#jobTitleInput').val('');
+    $('#annualSalaryInput').val('');
+  }
 }
+
+function deleteEmployee() {}
