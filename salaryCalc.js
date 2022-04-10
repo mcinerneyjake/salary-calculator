357b91ed71f9485c6251c890b9d0379dae4c8af5
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-10 18:30:01 -0500

    refactored .js file

--- salaryCalc.js
+++ salaryCalc.js
@@ -6,10 +6,11 @@ function onReady() {
 }
 
 let employees = [];
+let employeesToDelete = [];
 let totalAnnualSalaryExpense = 0;
+let totalMonthlySalaryExpense;
 
 function submitEmployee() {
-  // Create employee object with necessary properties
   let employeeObject = {
     firstName: $('#firstNameInput').val(),
     lastName: $('#lastNameInput').val(),
@@ -18,18 +19,14 @@ function submitEmployee() {
     annualSalary: $('#annualSalaryInput').val(),
   };
 
-  // Push employee object into employees array
-
   employees.push(employeeObject);
 
-  // Loop through each entered employee
+  employeesToDelete.push(employeeObject);
+
   let table = $('#tableRows');
   let employeeDeleteButton = `<button class="employeeDeleteButton">Delete</button>`;
 
   for (let employee of employees) {
-    // Give each <td> tag a class
-    // Select the <td> class via jQuery
-    // Prevent user error with inputs
     if (
       employee.firstName === '' ||
       employee.lastName === '' ||
@@ -50,11 +47,6 @@ function submitEmployee() {
   </tr>
   `);
 
-      // Give the annualSalary a special class and add each appended annualSalary class
-      // together to get the total salary for all employees
-
-      // If totalMonthlySalaryExpense > $20,000, turn backgroud color of #monthlyCostAmount to red.
-
       let totalMonthlySalaryExpense = (totalAnnualSalaryExpense += parseInt(employee.annualSalary)) / 12;
       $('#monthlyCostAmount').empty();
       $('#monthlyCostAmount').append(totalMonthlySalaryExpense.toFixed(2));
@@ -63,20 +55,16 @@ function submitEmployee() {
         $('#monthlyCostAmount').css('color', 'red');
       }
     }
-
-    // Empty employees array
-    employees = [];
-
-    // Empty text inputs
-    $('#firstNameInput').val('');
-    $('#lastNameInput').val('');
-    $('#idNumberInput').val('');
-    $('#jobTitleInput').val('');
-    $('#annualSalaryInput').val('');
   }
+  employees = [];
+
+  $('#firstNameInput').val('');
+  $('#lastNameInput').val('');
+  $('#idNumberInput').val('');
+  $('#jobTitleInput').val('');
+  $('#annualSalaryInput').val('');
 }
 
 function deleteEmployee() {
   $(this).closest('tr').remove();
-  //   $('#monthlyCostAmount');
 }
